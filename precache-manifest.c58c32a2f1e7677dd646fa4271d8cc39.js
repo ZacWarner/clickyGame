7f9d1acf38c5c155940ed5f0fd44b35dbf058dd7
ZacWarner <zektgn@gmail.com>
2019-07-27 23:54:24 -0700

    Updates

---
+++ precache-manifest.c58c32a2f1e7677dd646fa4271d8cc39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19056d113e547f0d6c311dd399710e53",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "e7e55344fdd7f188faca",
+    "url": "/clickyGame/static/css/main.1ac437ed.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickyGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "e7e55344fdd7f188faca",
+    "url": "/clickyGame/static/js/main.60acd88b.chunk.js"
+  },
+  {
+    "revision": "95f5ce002aae55a30c31",
+    "url": "/clickyGame/static/js/runtime~main.d5f62368.js"
+  },
+  {
+    "revision": "7ad7bbbb39ce05e47e6b428994b158c6",
+    "url": "/clickyGame/static/media/jumboBack.7ad7bbbb.jpg"
+  }
+]);
